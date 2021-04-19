dbf8a919e3b01cdfecd5a12b74078db028088f6a
parhanhambali <parhanhambali13@gmail.com>
2021-04-19 13:03:33 +0700

    flutter basic

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:cozy/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+    );
+  }
+}
